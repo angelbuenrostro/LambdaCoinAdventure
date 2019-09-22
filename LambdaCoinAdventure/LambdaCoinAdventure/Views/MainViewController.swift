7e91597ec301c59188fced81970c6961dd6c00b4
Angel <angelbuenrostro@outlook.com>
2019-09-22 16:48:15 -0400

    ADD - IBOutlets/Actions: Buttons

--- LambdaCoinAdventure/LambdaCoinAdventure/Views/MainViewController.swift
+++ LambdaCoinAdventure/LambdaCoinAdventure/Views/MainViewController.swift
@@ -10,24 +10,65 @@ import UIKit
 import SpriteKit
 
 class MainViewController: UIViewController {
+    
+    let apiController = APIController()
 
     @IBOutlet weak var mapView: MapView!
     
+    @IBOutlet weak var upButton: UIButton!
+    @IBOutlet weak var leftButton: UIButton!
+    @IBOutlet weak var rightButton: UIButton!
+    @IBOutlet weak var downButton: UIButton!
+    
+    @IBAction func upButtonPressed(_ sender: UIButton) {
+        print("north")
+        
+        apiController.move(direction: "n") { (result) in
+            if let room = try? result.get() {
+                DispatchQueue.main.async {
+                    <#code#>
+                }
+            }
+        }
+    }
+    @IBAction func rightButtonPressed(_ sender: UIButton) {
+        print("east")
+        apiController.move(direction: "e", completion: <#T##(Result<Room, NetworkError>) -> Void#>)
+    }
+    @IBAction func leftButtonPressed(_ sender: UIButton) {
+        print("west")
+        apiController.move(direction: "w", completion: <#T##(Result<Room, NetworkError>) -> Void#>)
+    }
+    @IBAction func downButtonPressed(_ sender: UIButton) {
+        print("south")
+        apiController.move(direction: "s", completion: <#T##(Result<Room, NetworkError>) -> Void#>)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = #colorLiteral(red: 0.07691047341, green: 0.0657993257, blue: 0.1335668266, alpha: 1)
         
         // Do any additional setup after loading the view.
-        setupMap()
+        setupUI()
         
     }
     
-    func setupMap(){
+    func setupUI(){
         // Round Map
         mapView.layer.opacity = 0.80
         mapView.layer.cornerRadius = 16.0
         mapView.clipsToBounds = true
+        
+        // Round Buttons
+        let buttonRadius = CGFloat(6.0)
+        upButton.layer.cornerRadius = buttonRadius
+        upButton.clipsToBounds = true
+        leftButton.layer.cornerRadius = buttonRadius
+        leftButton.clipsToBounds = true
+        rightButton.layer.cornerRadius = buttonRadius
+        rightButton.clipsToBounds = true
+        downButton.layer.cornerRadius = buttonRadius
+        downButton.clipsToBounds = true
     }
     
     
