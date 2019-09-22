a0601586caeefb3eccc420456cfe3319998941f9
Angel <angelbuenrostro@outlook.com>
2019-09-22 08:58:17 -0400

    ADD - Constraints to Map View

---
+++ LambdaCoinAdventure/LambdaCoinAdventure/Views/Support Views/MapView.swift
@@ -0,0 +1,21 @@
+//
+//  MapView.swift
+//  LambdaCoinAdventure
+//
+//  Created by Angel Buenrostro on 9/22/19.
+//  Copyright © 2019 Angel Buenrostro. All rights reserved.
+//
+
+import UIKit
+
+class MapView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
