9f343363faa82bfea0ceef0d61d87cfd56be6a08
Angel <angelbuenrostro@outlook.com>
2019-09-22 22:10:01 -0400

    ADD - IBOutlet/Action: Start Button Initializes Player in Maze

--- LambdaCoinAdventure/LambdaCoinAdventure/Views/MainViewController.swift
+++ LambdaCoinAdventure/LambdaCoinAdventure/Views/MainViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class MainViewController: UIViewController {
     
     // MARK: - Properties
+    
     let apiController = APIController()
     var currentRoom = Room(room_id: -1,
                            title: "testTitle",
@@ -22,50 +23,66 @@ class MainViewController: UIViewController {
                            errors: [],
                            messages: ["testMessage"]) {
         didSet {
-            self.view.setNeedsLayout()
+            updateUI()
+            self.view.setNeedsDisplay()
         }
     }
     
     // MARK: - Outlets
-
-    @IBOutlet weak var mapView: MapView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var idLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var messagesLabel: UILabel!
+    
     @IBOutlet weak var upButton: UIButton!
     @IBOutlet weak var leftButton: UIButton!
     @IBOutlet weak var rightButton: UIButton!
     @IBOutlet weak var downButton: UIButton!
     
+    @IBOutlet weak var startButton: UIButton!
+    
+    @IBOutlet weak var mapView: MapView!
     // MARK: - Actions
-    @IBAction func upButtonPressed(_ sender: UIButton) {
-        movePlayer("n")
-    }
+    @IBAction func upButtonPressed(_ sender: UIButton) {    movePlayer("n") }
+    @IBAction func rightButtonPressed(_ sender: UIButton) { movePlayer("e") }
+    @IBAction func leftButtonPressed(_ sender: UIButton) {  movePlayer("w") }
+    @IBAction func downButtonPressed(_ sender: UIButton) {  movePlayer("s") }
     
-    @IBAction func rightButtonPressed(_ sender: UIButton) {
-        movePlayer("e")
+    @IBAction func startButtonPressed(_ sender: UIButton) {
+        print("Start")
+        apiController.initialize { (result) in
+            if let room = try? result.get() {
+                
+                print("Room: \(room)")
+                print(room.errors.isEmpty)
+                DispatchQueue.main.async {
+                    if room.errors.isEmpty {
+                        // Sets returned API room as currentRoom triggering a didSet UI update
+                        self.currentRoom = room
+                    } else {
+                        print("Room: \(room)")
+                        print("Room Error: \(room.errors)")
+                    }
+                }
+            }
+        }
     }
-    
-    @IBAction func leftButtonPressed(_ sender: UIButton) {
-        movePlayer("w")
-    }
-    
-    @IBAction func downButtonPressed(_ sender: UIButton) {
-        movePlayer("s")
-    }
-    
     
     // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
         // Do any additional setup after loading the view.
         setupUI()
-        
     }
     
     
     // MARK: - Private Functions
     
     private func setupUI(){
+        // Initial UI Setup
+        updateUI()
         // Background Color
         self.view.backgroundColor = #colorLiteral(red: 0.07691047341, green: 0.0657993257, blue: 0.1335668266, alpha: 1)
         
@@ -84,23 +101,46 @@ class MainViewController: UIViewController {
         rightButton.clipsToBounds = true
         downButton.layer.cornerRadius = buttonRadius
         downButton.clipsToBounds = true
+        startButton.layer.cornerRadius = buttonRadius
+        startButton.clipsToBounds = true
+    }
+    
+    func updateUI() {
+        
+        // Formats array of string messages from Room Object into single string
+        var msgString = ""
+        if !currentRoom.messages.isEmpty {
+            for message in currentRoom.messages {
+                msgString.append(contentsOf: message)
+                msgString.append(", ")
+            }
+            msgString.removeLast(2)
+        } else {
+            msgString = "No messages"
+        }
+        
+        // Set Labels
+        self.messagesLabel.text = msgString
+        self.titleLabel.text = currentRoom.title
+        self.idLabel.text = ("ID: \(String(currentRoom.room_id))")
+        self.descriptionLabel.text = currentRoom.description
     }
     
     private func movePlayer(_ direction: String) {
         print(direction)
-//        apiController.move(direction: direction) { (result) in
-//            if let room = try? result.get() {
-//                DispatchQueue.main.async {
-//                    if room.errors.isEmpty {
-//                        // Sets returned API room as currentRoom triggering a didSet UI update
-//                        self.currentRoom = room
-//                    } else {
-//                        print("Room: \(room)")
-//                        print("Room Error: \(room.errors)")
-//                    }
-//                }
-//            }
-//        }
+        apiController.move(direction: direction) { (result) in
+            if let room = try? result.get() {
+                DispatchQueue.main.async {
+                    if room.errors.isEmpty {
+                        // Sets returned API room as currentRoom triggering a didSet UI update
+                        self.currentRoom = room
+                    } else {
+                        print("Room: \(room)")
+                        print("Room Error: \(room.errors)")
+                    }
+                }
+            }
+        }
     }
     
 
