3310a3e2bca223b292011f9f0b98ba2a091edcae
Angel <angelbuenrostro@outlook.com>
2019-09-22 20:06:05 -0400

    ADD - Buttons IBAction Function: Player Movement

--- LambdaCoinAdventure/LambdaCoinAdventure/Views/MainViewController.swift
+++ LambdaCoinAdventure/LambdaCoinAdventure/Views/MainViewController.swift
@@ -7,53 +7,68 @@
 //
 
 import UIKit
-import SpriteKit
 
 class MainViewController: UIViewController {
     
+    // MARK: - Properties
     let apiController = APIController()
+    var currentRoom = Room(room_id: -1,
+                           title: "testTitle",
+                           description: "testDescription",
+                           coordinates: "(1,1)",
+                           items: nil,
+                           exits: ["w","e"],
+                           cooldown: 1.0,
+                           errors: [],
+                           messages: ["testMessage"]) {
+        didSet {
+            self.view.setNeedsLayout()
+        }
+    }
+    
+    // MARK: - Outlets
 
     @IBOutlet weak var mapView: MapView!
-    
     @IBOutlet weak var upButton: UIButton!
     @IBOutlet weak var leftButton: UIButton!
     @IBOutlet weak var rightButton: UIButton!
     @IBOutlet weak var downButton: UIButton!
     
+    // MARK: - Actions
     @IBAction func upButtonPressed(_ sender: UIButton) {
-        print("north")
-        
-        apiController.move(direction: "n") { (result) in
-            if let room = try? result.get() {
-                DispatchQueue.main.async {
-                    <#code#>
-                }
-            }
-        }
+        movePlayer("n")
     }
+    
     @IBAction func rightButtonPressed(_ sender: UIButton) {
-        print("east")
-        apiController.move(direction: "e", completion: <#T##(Result<Room, NetworkError>) -> Void#>)
+        movePlayer("e")
     }
+    
     @IBAction func leftButtonPressed(_ sender: UIButton) {
-        print("west")
-        apiController.move(direction: "w", completion: <#T##(Result<Room, NetworkError>) -> Void#>)
+        movePlayer("w")
     }
+    
     @IBAction func downButtonPressed(_ sender: UIButton) {
-        print("south")
-        apiController.move(direction: "s", completion: <#T##(Result<Room, NetworkError>) -> Void#>)
+        movePlayer("s")
     }
+    
+    
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = #colorLiteral(red: 0.07691047341, green: 0.0657993257, blue: 0.1335668266, alpha: 1)
         
         // Do any additional setup after loading the view.
         setupUI()
         
     }
     
-    func setupUI(){
+    
+    // MARK: - Private Functions
+    
+    private func setupUI(){
+        // Background Color
+        self.view.backgroundColor = #colorLiteral(red: 0.07691047341, green: 0.0657993257, blue: 0.1335668266, alpha: 1)
+        
         // Round Map
         mapView.layer.opacity = 0.80
         mapView.layer.cornerRadius = 16.0
@@ -71,7 +86,22 @@ class MainViewController: UIViewController {
         downButton.clipsToBounds = true
     }
     
-    
+    private func movePlayer(_ direction: String) {
+        print(direction)
+//        apiController.move(direction: direction) { (result) in
+//            if let room = try? result.get() {
+//                DispatchQueue.main.async {
+//                    if room.errors.isEmpty {
+//                        // Sets returned API room as currentRoom triggering a didSet UI update
+//                        self.currentRoom = room
+//                    } else {
+//                        print("Room: \(room)")
+//                        print("Room Error: \(room.errors)")
+//                    }
+//                }
+//            }
+//        }
+    }
     
 
     /*
